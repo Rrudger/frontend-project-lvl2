fb33bef5fe7dc88af4acb1fe4e498d3a6655096d
Rrudger <soulo@yandex.ru>
2022-05-26 17:17:40 +0200

    update github actions linter

--- bin/gendiffFunction.js
+++ bin/gendiffFunction.js
@@ -1,17 +1,11 @@
 import { readFileSync } from 'fs';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { dirname} from 'path';
 import path from 'path';
 import _ from 'lodash';
-import { cwd } from 'node:process';
-
 
 export default function gendiff(path1, path2) {
   const obj1 = JSON.parse(readFileSync(path.resolve('__fixtures__', path1.split('/').pop())));
   const obj2 = JSON.parse(readFileSync(path.resolve('__fixtures__', path2.split('/').pop())));
-  const arr =_.sortBy(_.uniq(Object.keys(obj1).concat(Object.keys(obj2))));
-
+  const arr = _.sortBy(_.uniq(Object.keys(obj1).concat(Object.keys(obj2))));
   const result = {};
   const objKeys1 = Object.keys(obj1);
   const objKeys2 = Object.keys(obj2);
